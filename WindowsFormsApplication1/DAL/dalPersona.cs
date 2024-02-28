784a34b38c23d6441100e02e8279ab9fbfe9d74b
Hilker Bazan Flores <201914341d@utea.edu.pe>
2024-02-28 11:46:55 -0500

    modificamos el comun, DAL y WPL

---
+++ WindowsFormsApplication1/DAL/dalPersona.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class dalPersona
+    {
+        public void insertarPersona(dalPersona pPersona)
+        {
+            using (var bd= new examenRecuEntities())
+            {
+                bd.dalPersona.add(pPersona);
+            }
+        }
+    }
+}
